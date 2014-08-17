fb8515cc3757fe577913be9c8b05a7ca3bb62228
Борис Крапортов <boris@MacBook-Pro-Boris.local>
2014-08-17 14:06:06 +0400

    added debug on subscription

--- lib/em-instagram/api/subscriptions.rb
+++ lib/em-instagram/api/subscriptions.rb
@@ -5,6 +5,7 @@ module EventMachine
       def subscribe_to(options)
         options = options.merge(:aspect => 'media', :callback_url => self.callback_url)
         request_body = default_params.merge(:aspect => 'media', :callback_url => self.callback_url).merge(options)
+        self.logger.debug("trying subscribe with", request_body.inspect)
         request = request(:post, "/v1/subscriptions", :body => request_body)
         request.errback { |error| self.logger.debug "subscription error: #{error}"; EventMachine::add_timer(15) { subscribe_to options } }
         request.callback { |response| self.logger.debug "next subscription..."; EventMachine::next_tick { subscribe_next } }
